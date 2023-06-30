bbb1c40ad0a60e3002280bf4e6328dc7cf76109d
DavitBoo <davidmarras@gmail.com>
2023-06-30 17:18:22 +0200

    dark mode button created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Components/Header";
 import About from "./Components/About";
 import Projects from "./Components/Projects";
 import styled from "styled-components";
 import Footer from "./Components/Footer";
 import Contact from "./Components/Contact";
+import DarkModeToggle from "./Components/Generics/DarkModeToggle";
 
 const StyledDiv = styled.div`
+  position: relative;
   .container {
     display: flex;
     flex-direction: column;
@@ -15,9 +17,21 @@ const StyledDiv = styled.div`
     gap: 4rem;
     padding: 0 5%;
   }
+
+  .dark-mode-toggle {
+    position: fixed;
+    top: 20px;
+    right: 20px;
+  }
 `;
 
 function App() {
+  const [darkMode, setDarkMode] = useState<boolean>(false);
+
+  const handleChange = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <StyledDiv className="App">
       <div className="container">
@@ -26,6 +40,7 @@ function App() {
         <Projects></Projects>
         <Contact></Contact>
         <Footer></Footer>
+        <DarkModeToggle darkMode={darkMode} setDarkMode={setDarkMode} handleChange={handleChange} />
       </div>
     </StyledDiv>
   );
---
+++ src/Components/Generics/DarkModeToggle.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import Icon from "@mdi/react";
+import { mdiWhiteBalanceSunny } from "@mdi/js";
+
+const ToggleBtn = styled.div`
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 56px;
+    height: 30px;
+  }
+
+  .switch input {
+    display: none;
+  }
+
+  .slider {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: var(--color-azul-electrico);
+    border-radius: 30px;
+    transition: 0.4s;
+  }
+
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 22px;
+    width: 22px;
+    left: 4px;
+    bottom: 4px;
+    background-color: white;
+    border-radius: 50%;
+    transition: 0.4s;
+  }
+
+  input:checked + .slider {
+    background-color: var(--color-naranja-vibrante);
+  }
+
+  input:checked + .slider:before {
+    transform: translateX(26px);
+  }
+
+  .toggle-icons {
+    display: flex;
+    justify-content: space-between;
+    width: 50px;
+    position: absolute;
+    top: 4px;
+    right: 4px;
+  }
+
+  .fa {
+    color: white;
+  }
+
+  .fa-sun {
+    display: none;
+  }
+
+  input:checked ~ .toggle-icons .fa-sun {
+    display: inline-block;
+  }
+
+  input:checked ~ .toggle-icons .fa-moon {
+    display: none;
+  }
+`;
+
+export default function DarkModeToggle({ darkMode, setDarkMode, handleChange }: any) {
+  return (
+    <ToggleBtn className="dark-mode-toggle">
+      <label className="switch">
+        <input type="checkbox" onChange={handleChange()} />
+        <span className="slider"></span>
+        {/* <span className="toggle-icons">
+          <i className="fa fa-sun"></i>
+          <i className="fa fa-moon"></i>
+        </span> */}
+      </label>
+    </ToggleBtn>
+  );
+}
